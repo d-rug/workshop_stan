606de822bb4ada69ef14f9e71addb4996250f238
Wes B <wbrooks@ucdavis.edu>
2023-10-25 11:38:42 -0700

    cleaned up the scratch code a bit

--- scratch/do_sampling.R
+++ scratch/do_sampling.R
@@ -1,17 +1,28 @@
+# import packages
 library(cmdstanr)
-library(rethinking)
 library(dplyr)
 library(tidyr)
 library(ggplot2)
+library(workshop.stan)
 
+# attach the old faithful data
 data(faithful)
 
+# plot the old faithful data
+plot(faithful)
+
+# compile the Stan model
+mix_model = cmdstan_model(stan_file="stan/bivariate_mixture.stan")
+
+# create a list of data for the model
 stan_data = list(
   N = nrow(faithful),
-  duration = scale(faithful$eruptions)[,1],
-  interval = scale(faithful$waiting)[,1])
+  # duration = scale(faithful$eruptions)[,1],
+  # interval = scale(faithful$waiting)[,1])
+  duration = faithful$eruptions,
+  interval = faithful$waiting)
 
-mix_model = cmdstan_model(stan_file="stan/example_model.stan")
+# sample the posterior for the model, given the data
 mix_fit = mix_model$sample(
   data=stan_data,
   chains=4,
@@ -21,13 +32,16 @@ mix_fit = mix_model$sample(
 )
 
 
-
+# extract posterior samples
 mix_df = as_draws_df(mix_fit)
 
+# call functions that get the posterior and
+# posterior predictive samples for plotting
 posterior_mix = old_faithful_posterior_samples(mix_df)
 post_pred_samples = old_faithful_posterior_predictive_samples(mix_df)
 
-
+# plot the data, with posterior samples of the cluster centers
+# and contours for the posterior predictive 95% intervals
 ggplot(as.data.frame(scale(faithful))) +
   aes(x=eruptions, y=waiting) +
   geom_point() +
